56ee928b6ed1c26bdfb1f19418e2ff900e67a747
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2013-03-25 22:48:18 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,108 @@
+[![Build Status](https://secure.travis-ci.org/markdalgleish/bespoke-vcr.png)](http://travis-ci.org/markdalgleish/bespoke-vcr)
+
+# bespoke-vcr
+
+### Recording and Playback for [Bespoke.js](https://github.com/markdalgleish/bespoke.js)
+
+Record and replay Bespoke.js slide interactions from local storage and JSON data.
+
+## Download
+
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/markdalgleish/bespoke.js/master/dist/bespoke-vcr.min.js
+[max]: https://raw.github.com/markdalgleish/bespoke.js/master/dist/bespoke-vcr.js
+
+## Basic Usage
+
+First, include both `bespoke.js` and `bespoke-vcr.js` in your page.
+
+Then, simply include the plugin when using the `from(selector[, plugins])` method.
+
+```js
+bespoke.horizontal.from(selector, {
+  vcr: true
+});
+```
+
+Press 'R' on your keyboard to toggle recording. Press 'S' to stop recording. Press 'P' to play the latest recording.
+
+Recordings are saved to local storage and persist between page loads.
+
+## Advanced Usage
+
+### Generating JSON Recordings
+
+Bespoke-vcr outputs to your JavaScript console during recording and playback.
+
+To see JSON representations of recordings, run one of the following commands in your console:
+
+```js
+// Latest recording:
+vcr.latest();
+
+// All recordings:
+vcr.all();
+```
+
+### Loading JSON Recordings
+
+Recordings can be provided on load via the 'recording' option:
+
+```js
+bespoke.horizontal.from(selector, {
+  vcr: {
+    recording: [
+      {
+        "command": "next",
+        "timeout": 10000
+      },
+      {
+        "command": "next",
+        "timeout": 20000
+      },
+      {
+        "command": "next",
+        "timeout": 30000
+      },
+      {
+        "timeout": 40000
+      }
+    ]
+  }
+});
+```
+
+Press 'P' to start the saved recording.
+
+### Custom Remotes
+
+The default remote uses the 'R', 'P' and 'S' keys.
+
+To override this behavious, pass a 'remote' function via the options object:
+
+```js
+bespoke.horizontal.from(selector, {
+  vcr: {
+    remote: function(remote) {
+      // Toggle recording:
+      remote.record();
+
+      // Stop recording
+      remote.stop();
+      
+      // Toggle plaback
+      remote.play();
+    }
+});
+```
+
+## Questions?
+
+Contact me on GitHub or Twitter: [@markdalgleish](http://twitter.com/markdalgleish)
+
+## License
+
+Copyright 2013, Mark Dalgleish  
+This content is released under the MIT license  
+http://markdalgleish.mit-license.org
---
+++ dist/bespoke-vcr.js
@@ -0,0 +1,188 @@
+/*!
+ * bespoke-vcr v0.0.1-alpha-1
+ *
+ * Copyright 2013, Mark Dalgleish
+ * This content is released under the MIT license
+ * http://mit-license.org/markdalgleish
+ */
+
+(function(bespoke) {
+
+	var plugin = function(deck, options) {
+			options.reporter = options.reporter || 'console';
+
+			var recordings = getRecordings() || [],
+				frames = options.recording || getLatestRecording(),
+				report = typeof options.reporter === 'string' ? plugin.reporters[options.reporter] : options.reporter,
+				recordStartTime,
+				isRecording = false,
+				isPlaying = false,
+
+				status = (function() {
+					var indicator = document.createElement('div'),
+						size = '8px';
+
+					indicator.style.width = size;
+					indicator.style.height = size;
+					indicator.style.borderRadius = size;
+					indicator.style.position = 'absolute';
+					indicator.style.left = '4px';
+					indicator.style.bottom = '4px';
+					indicator.style.backgroundColor = 'transparent';
+					document.body.appendChild(indicator);
+
+					return {
+						red: function() {
+							indicator.style.backgroundColor = 'red';
+						},
+						green: function() {
+							indicator.style.backgroundColor = 'green';
+						},
+						clear: function() {
+							indicator.style.backgroundColor = 'transparent';
+						}
+					};
+				}()),
+
+				record = function() {
+					if (isRecording) {
+						return stop();
+					}
+
+					deck.slide(0);
+
+					isRecording = true;
+					isPlaying = false;
+					status.red();
+
+					report('Recording to local storage...');
+					recordStartTime = new Date().getTime();
+
+					frames = [];
+					recordings.push(frames);
+				},
+
+				play = function() {
+					isRecording && stop();
+
+					if (frames.length === 0) {
+						return;
+					}
+
+					isPlaying = true;
+					status.green();
+
+					report('Playing the following recording:', frames);
+
+					deck.slide(0);
+					frames.forEach(function(frame) {
+						setTimeout(function() {
+							if (frame.command) {
+								deck[frame.command].apply(null, frame.arguments || []);
+							} else {
+								report('Playback complete');
+								isPlaying = false;
+								status.clear();
+							}
+						}, frame.timeout);
+					});
+				},
+
+				stop = function() {
+					if (isRecording) {
+						frames.push({
+							timeout: new Date().getTime() - recordStartTime
+						});
+						save(recordings);
+						report('Successfully recorded the following to local storage:', frames);
+					}
+
+					isRecording = false;
+					isPlaying = false;
+					status.clear();
+				};
+
+			['next', 'prev', 'slide'].forEach(function(command) {
+				deck.on(command, function(e) {
+					var frame = {
+						command: command,
+						timeout: new Date().getTime() - recordStartTime
+					};
+
+					if (command === 'slide') {
+						frame.arguments = [e.index];
+					}
+
+					isRecording && frames.push(frame) && save(recordings);
+				});
+			});
+
+			var setupRemote = options.remote || function(remote) {
+					window.addEventListener('keydown', function(e) {
+						var P = 80,
+							R = 82,
+							S = 83;
+
+						switch (e.which) {
+							case R:
+								remote.record();
+								break;
+							case S:
+								remote.stop();
+								break;
+							case P:
+								remote.play();
+								break;
+						}
+					});
+				},
+				
+				controls = {
+					record: record,
+					play: play,
+					stop: stop
+				};
+
+			setupRemote(controls);
+		},
+
+		getRecordings = function() {
+			var recordings = localStorage['bespoke-vcr'];
+			return recordings ? JSON.parse(recordings) : [];
+		},
+
+		getLatestRecording = function() {
+			var recordings = getRecordings();
+			return recordings.length ? recordings[recordings.length - 1] : undefined;
+		},
+
+		save = function(recordings) {
+			localStorage['bespoke-vcr'] = JSON.stringify(recordings);
+		},
+
+		clear = function() {
+			delete localStorage['bespoke-vcr'];
+		};
+
+	plugin.reporters = {
+		console: function(title, data) {
+			console.log(
+				'BESPOKE-VCR: ' + title + '\n' +
+				(data ? JSON.stringify(data, null, 2) + '\n' : '')
+			);
+		}
+	};
+
+	bespoke.plugins.vcr = plugin;
+
+	window.vcr = {
+		latest: function() {
+			plugin.reporters.console('Latest recording:', getLatestRecording());
+		},
+		all: function() {
+			plugin.reporters.console('All recordings:', getRecordings());
+		},
+		clear: clear
+	};
+
+}(bespoke));
